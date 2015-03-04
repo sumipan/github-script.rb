159e5c537cfcce2543f6465b97222db77ba97bc1
takashi nagayasu <regist@g-onion.org>
2015-03-04 18:20:05 +0900

    Add test

--- test/test_script.rb
+++ test/test_script.rb
@@ -9,9 +9,28 @@ describe Github::Script do
   end
   
   it "should create User class instance" do
-    Github::Script.proc(@text) do
+    skip "conflict other test"
+    Github::Script.serve(Github::Script.scan(@text)) do
       User.class.must_equal Class
       User.new.must_be_instance_of User
+    end
+  end
+  
+  it "ok" do
+    @options = {
+      :user => ENV['GITHUB_USER'],
+      :repo => ENV['GITHUB_REPO'],
+    }
+  
+    Github::Script.configure do |github|
+      github.setup(@options.merge({
+        :oauth_token => ENV['GITHUB_TOKEN']
+      }))
+    end
+    
+    recipe = Github::Script.discover(ENV['GITHUB_ISSUE'])
+    Github::Script.serve(recipe) do
+      $server.must_equal "dev02"
     end
   end
 end
