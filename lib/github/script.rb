c808c7daf713819d4ae885b80a6c36e7255b4821
takashi nagayasu <regist@g-onion.org>
2015-03-04 18:19:49 +0900

    discover github issue

--- lib/github/script.rb
+++ lib/github/script.rb
@@ -1,31 +1,44 @@
 require "github/script/version"
+require "github_api"
 
 module Github
   module Script
-    def proc(text, *args, &block)
-      raise "error. text is null" unless text
-
-      eval scan(text), TOPLEVEL_BINDING
-      self.instance_eval &block
+    def configure(&block)
+      @github = Github::Client.new
+      yield @github
+    end
+    
+    def discover(number)
+      raise "error. text is null" unless number
+      text = @github.issues.get(:number => number).body.body
+      scan(text)
+    end
+    
+    def serve(text, &block)
+      eval text, TOPLEVEL_BINDING
+      self.instance_eval &block if block_given?
     end
 
-    def self.scan(text)
+    def scan(text)
       inside = false
       script = ""
 
       text.each_line do |line|
-        if  line.match('^```script') then
+        if  line.match(/^```script/) then
           inside = true 
-        elsif line.match('^```') then
+        elsif line.match(/^```/) then
           inside = false 
         elsif inside then
           script += line
+        elsif match = line.match(/#(\d+)/) then
+          text = @github.issues.get(:number => match[1].to_i).body.body
+          script += scan(text)
         end
       end
 
       script
     end
     
-    module_function :proc
+    module_function :configure, :discover, :serve, :scan
   end
 end
